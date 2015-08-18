d9daddf8c29474e0daa43bcf3f3e93b9d2b2bd45
Rob Sanchez <rob@robsanchez.com>
2015-08-18 18:17:47 -0500

    First commit

---
+++ craft-cli.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class CraftCli < Formula
+  homepage 'https://github.com/rsanchez/craft-cli'
+  url 'https://github.com/rsanchez/craft-cli/releases/download/0.0.0/craft.phar'
+  sha1 '0000000000000000000000000000000000000000'
+  version '0.0.0'
+
+  def install
+    libexec.install "craft.phar"
+    sh = libexec + "craft"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/craft.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  test do
+    system 'craft --version'
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "craft --version".
+
+    You can read more about craft-cli by running:
+      "brew home craft-cli".
+    EOS
+  end
+
+end
