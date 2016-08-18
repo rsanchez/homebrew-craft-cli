02eff78f36b4aee18ee17ef34abdd4527a18b9d9
Rob Sanchez <rob@robsanchez.com>
2016-08-18 13:48:45 -0500

    Release 0.7.1

--- craft-cli.rb
+++ craft-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class CraftCli < Formula
   homepage 'https://github.com/rsanchez/craft-cli'
-  url 'https://github.com/rsanchez/craft-cli/releases/download/0.7.0/craft.phar'
-  sha256 '736c7402ca19075848b13048c9eb6bef466329d88a3c08aae5ceb698b5b6b387'
-  version '0.7.0'
+  url 'https://github.com/rsanchez/craft-cli/releases/download/0.7.1/craft.phar'
+  sha256 '5fba6132f2f1a858ae9565e6630a969c5d741694fdc6695601914c570e287d8b'
+  version '0.7.1'
 
   def install
     libexec.install "craft.phar"
